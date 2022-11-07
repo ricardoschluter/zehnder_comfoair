a7de9bf65c4a484c503dacac19cf206240c57049
SCHLUR01 <ricardo.schluter@heineken.com>
2022-11-07 11:10:40 +0100

    Another update

--- components/zehnder_comfoair/zehnder_comfoair.cpp
+++ components/zehnder_comfoair/zehnder_comfoair.cpp
@@ -11,12 +11,13 @@ namespace esphome {
         {
 
         }
-        class ComfoAir: public PollingComponent
+        class ComfoAir: public PollingComponent, public Sensor
         {
             public:
               ComfoAir() : PollingComponent(10000){}
               void setup() override{}
               void update() override{
+                publish_state(42.0);
                 ESP_LOGD("custom","Logging stuff");
               }
         }
