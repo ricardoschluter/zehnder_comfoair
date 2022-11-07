d4c28f5fff30fa60b57568ee76f63d6421002e1b
SCHLUR01 <ricardo.schluter@heineken.com>
2022-11-07 14:40:29 +0100

    b

--- components/zehnder_comfoair/zehnder_comfoair.h
+++ components/zehnder_comfoair/zehnder_comfoair.h
@@ -1,11 +1,13 @@
 #pragma once
 
+#include "esphome.h"
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
 
 class EmptyUARTComponent : public UARTDevice, public Component {
   public:
-    EmptyUARTComponent(UARTComponent *parent) : UARTDevice(parent) {}
+    EmptyUARTComponent(UARTComponent *parent) : UARTDevice(parent){}
+     Sensor *distance_sensor = new Sensor();
     void setup() override;
     void loop() override;
     void dump_config() override;
