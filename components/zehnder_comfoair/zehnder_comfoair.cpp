2b64cd38f7f193b28372016b23eaf34006d7b0cb
SCHLUR01 <ricardo.schluter@heineken.com>
2022-11-07 12:29:55 +0100

    pp

--- components/zehnder_comfoair/zehnder_comfoair.cpp
+++ components/zehnder_comfoair/zehnder_comfoair.cpp
@@ -7,7 +7,8 @@ namespace zehnder_comfoair {
 static const char *TAG = "zehnder_comfoair.component";
 
 void Comfoair::setup() {
-
+   LOG_PIN("  TX Pin: ", tx_pin_);
+   LOG_PIN("  RX Pin: ", rx_pin_);
 }
 
 void Comfoair::update() {
